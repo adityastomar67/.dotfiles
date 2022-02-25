61bd11a893831f782af0e7c4caafdb4ce79ad612
Aditya Singh Tomar <adityastomar12@gmail.com>
2022-02-25 10:54:10 +0530

    Update

--- init.sh
+++ init.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-
+cp ~/.dotfiles/list.pacman $HOME
+cd $HOME
+sudo pacman -Syu
 sudo pacman -S - < list.pacman
 
-fonts
-wall
-apps
-vscode
+cp ~/.dotfiles/.scripts/text.txt $HOME
+cp ~/.dotfiles/.scripts/stw $HOME
+cd $HOME
+chmod +x ./stw
+./stw
+
+cd ~/.dotfiles/.scripts
+./fonts
+./wall
+# apps
+./vscode
 
 # For Changing the screen timeout to one hour
 xset s 3600 3600
