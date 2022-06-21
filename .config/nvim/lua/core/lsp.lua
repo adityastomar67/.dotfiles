29cc8f92a477e2775b58665a583c77daa4f40ef3
Aditya Singh Tomar <adityastomar12@gmail.com>
2022-06-21 09:01:21 +0530

    LSP Updates

--- .config/nvim/lua/core/lsp.lua
+++ .config/nvim/lua/core/lsp.lua
@@ -3,17 +3,47 @@ local function custom_codeAction_callback(_, _, action)
 end
 
 local servers = {
-	"pyright",
-	"clangd",
-	"bashls",
-	"sqls",
-	"sumneko_lua",
-	"jsonls",
-	"eslint",
-    -- "emmet_ls"
-	-- "tsserver",
-	-- "tailwindcss",
-	-- "jdtls",
+	pyright = {},
+	clangd = {},
+	bashls = {},
+	sqls = {},
+	sumneko_lua = {
+		settings = {
+			Lua = {
+				runtime = {
+					-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+					version = "LuaJIT",
+					-- Setup your lua path
+					path = vim.split(package.path, ";"),
+				},
+				diagnostics = {
+					-- Get the language server to recognize the `vim` global
+					globals = { "vim", "describe", "it", "before_each", "after_each", "packer_plugins" },
+					disable = {
+						"lowercase-global",
+						"undefined-global",
+						"unused-local",
+						"unused-vararg",
+						"trailing-space",
+					},
+				},
+				workspace = {
+					-- Make the server aware of Neovim runtime files
+					library = {
+						[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+						[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+					},
+					-- library = vim.api.nvim_get_runtime_file("", true),
+					maxPreload = 2000,
+					preloadFileSize = 50000,
+				},
+				completion = { callSnippet = "Both" },
+				telemetry = { enable = false },
+			},
+		},
+	},
+	jsonls = {},
+	eslint = {},
 }
 
 local opts = { noremap = true, silent = true }
@@ -50,11 +80,17 @@ local on_attach = function(client, bufnr)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+	vim.api.nvim_buf_set_keymap(
+		bufnr,
+		"n",
+		"<space>wl",
+		"<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
+		opts
+	)
 end
 
 for _, lsp in pairs(servers) do
-	require("lspconfig")[lsp].setup({
+	require("lspconfig")[_].setup({
 		on_attach = on_attach,
 		flags = {
 			-- This will be the default in neovim 0.7+
