fe002fb980ebf07aafd001e39f9d5da65ff59471
Aditya Singh Tomar <adityastomar12@gmail.com>
2022-07-22 12:11:00 +0530

    Conceal automatic removal on mouse hover

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -21,3 +21,21 @@ vim.cmd('source $HOME/.abbreviations.vim')
 require("notify").setup({
   background_colour = "#000000",
 })
+
+local opts = { noremap = true, silent = true }
+
+vim.keymap.set("n",  "<F10>" , function()
+	if vim.o.conceallevel > 0 then
+		vim.o.conceallevel = 0
+	else
+		vim.o.conceallevel = 2
+	end
+end, opts)
+
+vim.keymap.set("n",  "<F11>" , function()
+	if vim.o.concealcursor == "n" then
+		vim.o.concealcursor = ""
+	else
+		vim.o.concealcursor = "n"
+	end
+end, opts)
