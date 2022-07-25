abf54c9799f48e3f6d9515f1d05079dd2955aed3
Aditya Singh Tomar <adityastomar12@gmail.com>
2022-07-25 13:31:10 +0530

    ADD: New Python Conceals & AutoCommands for NVIM

--- .config/nvim/lua/core/cmds.lua
+++ .config/nvim/lua/core/cmds.lua
@@ -28,30 +28,30 @@ function nvim_create_augroups(definitions)
     end
 end
 
-vim.cmd([[autocmd InsertEnter * norm zz]]) -- Vertically center document when entering insert mode
--- cmd([[Command! Realtime set autoread | ]]) -- Automatic update of any Buffer when not in use
-vim.cmd([[cnoreab cls Cls]])
+vim.cmd([[autocmd InsertEnter * norm zz]])                                                    -- Vertically center document when entering insert mode
+vim.cmd([[command! Realtime set autoread | au CursorHold * checktime | call feedkeys("lh")]]) -- Automatic update of any Buffer when not in use
 vim.cmd([[command! Cls lua require("core.utils").preserve('%s/\\s\\+$//ge')]])
 vim.cmd([[command! Reindent lua require('core.utils').preserve("sil keepj normal! gg=G")]])
 vim.cmd([[command! BufOnly lua require('core.utils').preserve("silent! %bd|e#|bd#")]])
+vim.cmd([[command! CloneBuffer new | 0put =getbufline('#',1,'$')]])
+vim.cmd([[command! Mappings drop ~/.config/nvim/lua/core/keymaps.lua]])
+vim.cmd([[command! Scratch new | setlocal bt=nofile bh=wipe nobl noswapfile nu]])
+vim.cmd([[command! Blockwise lua require('core.utils').blockwise_clipboard()]])
+vim.cmd([[command! SaveAsRoot w !doas tee %]])
+vim.cmd([[command! ReloadConfig lua require("utils").ReloadConfig()]])
+vim.cmd([[cnoreab cls Cls]])
 vim.cmd([[cnoreab Bo BufOnly]])
 vim.cmd([[cnoreab W w]])
 vim.cmd([[cnoreab W! w!]])
-vim.cmd([[command! CloneBuffer new | 0put =getbufline('#',1,'$')]])
-vim.cmd([[command! Mappings drop ~/.config/nvim/lua/user/mappings.lua]])
-vim.cmd([[command! Scratch new | setlocal bt=nofile bh=wipe nobl noswapfile nu]])
-vim.cmd([[syntax sync minlines=64]]) --  faster syntax hl
-vim.cmd([[command! Blockwise lua require('core.utils').blockwise_clipboard()]])
 vim.cmd([[cnoreab Bw Blockwise]])
-vim.cmd([[cmap w!! w !doas tee % >/dev/null]]) -- save as root, in my case I use the command 'doas'
-vim.cmd([[command! SaveAsRoot w !doas tee %]])
-vim.cmd('command! ReloadConfig lua require("utils").ReloadConfig()')
+vim.cmd([[cnoreab cls Cls]])
 vim.cmd([[inoreab Fname <c-r>=expand("%:p")<cr>]])
 vim.cmd([[inoreab Iname <c-r>=expand("%:p")<cr>]])
 vim.cmd([[inoreab fname <c-r>=expand("%:t")<cr>]])
 vim.cmd([[inoreab iname <c-r>=expand("%:t")<cr>]])
 vim.cmd([[inoreabbrev idate <C-R>=strftime("%b %d %Y %H:%M")<CR>]])
-vim.cmd([[cnoreab cls Cls]])
+vim.cmd([[syntax sync minlines=64]])           -- faster syntax hl
+vim.cmd([[cmap w!! w !doas tee % >/dev/null]]) -- save as root, in my case I use the command 'doas'
 vim.cmd([[set iskeyword+=-]])
 -- vim.cmd("hi normal guibg=NONE ctermbg=NONE")
 -- vim.cmd([[command! -bar -nargs=1 Grep silent grep <q-args> | redraw! | cw]])
@@ -175,11 +175,7 @@ local function code_keymap()
 end
 
 local autocmds = {
-    -- autoskel = {
-        -- {"BufNewFile", "*.lua,*.sh", 'lua vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("i_skel<CR>",true,false,true),"m",true)' },
-    -- },
     reload_vimrc = {
-        -- Reload vim config automatically
         -- {"BufWritePost",[[$VIM_PATH/{*.vim,*.yaml,vimrc} nested source $MYVIMRC | redraw]]};
         { "BufWritePre", "$MYVIMRC", "lua require('core.utils').ReloadConfig()" },
     },
@@ -187,16 +183,10 @@ local autocmds = {
         { "Filetype", "qf,help,man,lspinfo", ":nnoremap <silent> <buffer> q :close<CR>" },
         { "Filetype", "qf", ":set nobuflisted" },
     },
-    -- https://www.reddit.com/r/vim/comments/4aab93/comment/d0yo622/
-    -- https://freshman.tech/vim-quickfix-and-location-list/
     autoquickfix = {
         { "QuickFixCmdPost", "[^l]*", "cwindow" },
         { "QuickFixCmdPost", "l*", "lwindow" },
     },
-    -- wrap_spell = {
-    --     { "FileType", "markdown", ":setlocal wrap" },
-    --     { "FileType", "markdown", ":setlocal spell" },
-    -- },
     fix_commentstring = {
         { "Bufenter", "*config,*rc,*conf", "set commentstring=#%s" },
         { "Bufenter", "*config,*conf,sxhkdrc,bspwmrc", "set syntax=config" },
@@ -214,12 +204,11 @@ local autocmds = {
         {'BufWritePost', '~/.Xdefaults', '!xrdb -merge ~/.Xdefaults'},
         {'BufWritePost', 'fonts.conf', '!fc-cache'},
     },
-	-- https://stackoverflow.com/a/23326474/2571881
 	format_options = { -- :h fo-talbe (for help)
 		{ "BufWinEnter,BufRead,BufNewFile", "*", "setlocal formatoptions-=r formatoptions-=o" },
 	},
     change_header = {
-        -- { "BufWinLeave", "*", "lua require('core.utils').changeheader()" },
+        { "BufWinLeave", "*", "lua require('core.utils').changeheader()" },
     },
     resize_windows_proportionally = {
         { "VimResized", "*", ":wincmd =" };
@@ -240,24 +229,15 @@ local autocmds = {
     restore_cursor = {
         { "BufRead", "*", [[call setpos(".", getpos("'\""))]] },
     },
-
-    -- https://vi.stackexchange.com/a/25687/7339
-    -- hicurrent_word = {
-    --     { "CursorHold", "*", [[:exec 'match Search /\V\<' . expand('<cword>') . '\>/']] },
-    -- },
     save_shada = {
         { "VimLeave", "*", "wshada!"};
         { "CursorHold", "*", [[rshada|wshada]]};
     };
-    -- auto_exit_insertmode = {
-    --     { "CursorHoldI", "*", "stopinsert" },
-    -- },
     wins = {
         -- { "VimResized", "*", ":wincmd =" },
         -- { "WinEnter", "*", "wincmd =" },
         { "BufEnter", "NvimTree", [[setlocal cursorline]] },
     },
-    -- show trailing spaces only in normal mode and activastes cursorline
     toggle_search_highlighting = {
         { "InsertEnter,InsertLeave", "*", [[set cul!]]},
         { "InsertEnter", "*", "setlocal nohlsearch" },
@@ -268,24 +248,32 @@ local autocmds = {
     lua_highlight = {
         { "TextYankPost", "*", [[silent! lua vim.highlight.on_yank() {higroup="IncSearch", timeout=600}]] },
     },
-    auto_working_directory = {
-        { "BufEnter", "*", "silent! lcd %:p:h" },
-    },
     clean_trailing_spaces = {
         { "BufWritePre", "*", [[lua require("core.utils").preserve('%s/\\s\\+$//ge')]] },
     },
+    -- autoskel = {
+        -- {"BufNewFile", "*.lua,*.sh", 'lua vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("i_skel<CR>",true,false,true),"m",true)' },
+    -- },
+    -- wrap_spell = {
+    --     { "FileType", "markdown", ":setlocal wrap" },
+    --     { "FileType", "markdown", ":setlocal spell" },
+    -- },
+    -- hicurrent_word = {
+    --     { "CursorHold", "*", [[:exec 'match Search /\V\<' . expand('<cword>') . '\>/']] },
+    -- },
+    -- auto_exit_insertmode = {
+    --     { "CursorHoldI", "*", "stopinsert" },
+    -- },
+    -- auto_working_directory = {
+    --     { "BufEnter", "*", "silent! lcd %:p:h" },
+    -- },
     -- ansi_esc_log = {
     --     { "BufEnter", "*.log", ":AnsiEsc" };
     -- };
-
-    -- https://stackoverflow.com/a/18427760/2571881
-    -- https://vimhelp.org/usr_11.txt.html
-    -- https://ttm.github.io/research/2017/11/02/vim-swp-swo.html
     -- AutoRecoverSwapFile = {
     --     { "SwapExists", "*", [[let v:swapchoice = 'r' | let b:swapname = v:swapname]] },
     --     { "BufWinEnter", "*", [[if exists("b:swapname") | call delete(b:swapname) | endif]] },
     -- },
-    -- flash cursorline and change cursor shape:
     -- flash_cursor_line = {
     --     { "WinEnter", "*", "lua require('core.utils').flash_cursorline()" },
     --     -- { "WinEnter", "*", "Beacon" },
@@ -298,28 +286,23 @@ local autocmds = {
     -- 	{"FileType", "conf", "ColorizerAttachToBuffer<CR>"};
     -- };
 }
-
 nvim_create_augroups(autocmds)
 -- autocommands END
 
 local augroups = {}
-
 augroups.buf_write_pre = {
-
     mkdir_before_saving = {
         event = {"BufWritePre", "FileWritePre"},
         pattern = "*",
         -- TODO: Replace vimscript function
         command = [[ silent! call mkdir(expand("<afile>:p:h"), "p") ]],
     },
-
     trim_extra_spaces_and_newlines = {
         event = "BufWritePre",
         pattern = "*",
         -- TODO: Replace vimscript function
         command = require("core.utils").preserve('%s/\\s\\+$//ge'),
     },
-
 }
 
 augroups.misc = {
@@ -347,6 +330,3 @@ augroups.misc = {
 		end
 	},
 }
-
--- TODO: To create a function that Updates a file in Realtime
-    -- NOTE: :set autoread | au CursorHold * checktime | call feedkeys("lh")
