c487c4e8c59fccb54afc5d51c13e99b6b03a3392
Aditya Singh Tomar <adityastomar12@gmail.com>
2022-10-21 19:19:34 +0530

    awesome fzf updates

--- .zsh/fzf.zsh
+++ .zsh/fzf.zsh
@@ -1,158 +1,159 @@
-AWESOME_FZF_LOCATION="~/.dotfiles/zsh/fzf.zsh"
+AWESOME_FZF_LOCATION="$HOME/.dotfiles/.zsh/fzf.zsh"
+RED_FG=`tput setaf 1`
+GREEN_BG=`tput setab 2`
+RESET=`tput sgr0`
 
 #List Awesome FZF Functions
 function fzf-awesome-list() {
-if [[ -f $AWESOME_FZF_LOCATION ]]; then
-    selected=$(grep -E "(function fzf-)(.*?)[^(]*" $AWESOME_FZF_LOCATION | sed -e "s/function fzf-//" | sed -e "s/() {//" | grep -v "selected=" | fzf --reverse --prompt="awesome fzf functions > ")
-else
-    echo "awesome fzf not found"
-fi
+    if [ -r $AWESOME_FZF_LOCATION ]; then
+        selected=$(grep -E "(function fzf-)(.*?)[^(]*" $AWESOME_FZF_LOCATION | sed -e "s/function fzf-//" | sed -e "s/() {//" | grep -v "selected=" | fzf --reverse --prompt="Awesome-FZF functions > ")
+    else
+        echo "${RED_FG}Awesome-FZF not found!!${RESET}"
+    fi
+
     case "$selected" in
-        "");; #don't throw an exit error when we dont select anything
+        "");;  #don't throw an exit error when we dont select anything
         *) "fzf-"$selected;;
     esac
 }
 
 #Enhanced rm
 function fzf-rm() {
-  if [[ "$#" -eq 0 ]]; then
-    local files
-    files=$(find . -maxdepth 1 -type f | fzf --multi)
-    echo $files | xargs -I '{}' rm {} #we use xargs so that filenames to capture filenames with spaces in them properly
-  else
-    command rm "$@"
-  fi
+    if [[ "$#" -eq 0 ]]; then
+        local files
+        files=$(find . -maxdepth 1 -type f | fzf --multi)
+        echo $files | xargs -I '{}' rm {}  #we use xargs so that filenames to capture filenames with spaces in them properly
+    else
+        command rm "$@"
+    fi
 }
 
 # Man without options will use fzf to select a page
 function fzf-man() {
-	MAN="/usr/bin/man"
-	if [ -n "$1" ]; then
-		$MAN "$@"
-		return $?
-	else
-		$MAN -k . | fzf --reverse --preview="echo {1,2} | sed 's/ (/./' | sed -E 's/\)\s*$//' | xargs $MAN" | awk '{print $1 "." $2}' | tr -d '()' | xargs -r $MAN
-		return $?
-	fi
+    MAN="/usr/bin/man"
+    if [ -n "$1" ]; then
+        $MAN "$@"
+        return $?
+    else
+        $MAN -k . | fzf --reverse --preview="echo {1,2} | sed 's/ (/./' | sed -E 's/\)\s*$//' | xargs $MAN" | awk '{print $1 "." $2}' | tr -d '()' | xargs -r $MAN
+        return $?
+    fi
 }
-
 
 #Eval commands on the fly
 function fzf-eval() {
-echo | fzf -q "$*" --preview-window=up:99% --preview="eval {q}"
+    echo | fzf -q "$*" --preview-window=up:99% --preview="eval {q}"
 }
 
 ## Search list of your aliases and functions
 function fzf-aliases-functions() {
     CMD=$(
-        (
-            (alias)
-            (functions | grep "()" | cut -d ' ' -f1 | grep -v "^_" )
-        ) | fzf | cut -d '=' -f1
+    (
+    (alias)
+    (functions | grep "()" | cut -d ' ' -f1 | grep -v "^_" )
+    ) | fzf | cut -d '=' -f1
     );
 
     eval $CMD
 }
 
-
-## File Finder (Open in $EDITOR)
+# File Finder (Open in $EDITOR)
 function fzf-find-files() {
-  local file=$(fzf --multi --reverse) #get file from fzf
-  if [[ $file ]]; then
-    for prog in $(echo $file); #open all the selected files
-    do; $EDITOR $prog; done;
-  else
-    echo "cancelled fzf"
-  fi
+    local file=$(fzf --multi --reverse)  #get file from fzf
+    if [[ $file ]]; then
+        for prog in $(echo $file);  #open all the selected files
+        do; $EDITOR $prog; done;
+    else
+        echo "${RED_FG}Cancelled FZF${RESET}"
+    fi
 }
-
-
 
 # Find Dirs
 function fzf-cd() {
-  local dir
-  dir=$(find ${1:-.} -path '*/\.*' -prune \
-                  -o -type d -print 2> /dev/null | fzf +m) &&
-  cd "$dir"
-  ls
+    local dir
+    dir=$(find ${1:-.} -path '*/\.*' -prune -o -type d -print 2> /dev/null | fzf +m) && cd "$dir"
+    if [ $(command -v exa) ]; then
+        clear && exa -al --color=always --icons --git --group-directories-first
+    else
+        command ls -al
+    fi
 }
 
 # Find Dirs + Hidden
 function fzf-cd-incl-hidden() {
-  local dir
-  dir=$(find ${1:-.} -type d 2> /dev/null | fzf +m) && cd "$dir"
-  ls
+    local dir
+    dir=$(find ${1:-.} -type d 2> /dev/null | fzf +m) && cd "$dir"
+    ls
 }
 
 # cd into the directory of the selected file
 function fzf-cd-to-file() {
-   local file
-   local dir
-   file=$(fzf +m -q "$1") && dir=$(dirname "$file") && cd "$dir"
-   ls
+    local file
+    local dir
+    file=$(fzf +m -q "$1") && dir=$(dirname "$file") && cd "$dir"
+    ls
 }
 
 # fdr - cd to selected parent directory
 function fzf-cd-to-parent() {
-  local declare dirs=()
-  get_parent_dirs() {
-    if [[ -d "${1}" ]]; then dirs+=("$1"); else return; fi
-    if [[ "${1}" == '/' ]]; then
-      for _dir in "${dirs[@]}"; do echo $_dir; done
-    else
-      get_parent_dirs $(dirname "$1")
-    fi
-  }
-  local DIR=$(get_parent_dirs $(realpath "${1:-$PWD}") | fzf-tmux --tac)
-  cd "$DIR"
-  ls
+    local declare dirs=()
+    get_parent_dirs() {
+        if [[ -d "${1}" ]]; then dirs+=("$1"); else return; fi
+        if [[ "${1}" == '/' ]]; then
+            for _dir in "${dirs[@]}"; do echo $_dir; done
+        else
+            get_parent_dirs $(dirname "$1")
+        fi
+    }
+    local DIR=$(get_parent_dirs $(realpath "${1:-$PWD}") | fzf-tmux --tac)
+    cd "$DIR"
+    ls
 }
 
 # Search env variables
 function fzf-env-vars() {
-  local out
-  out=$(env | fzf)
-  echo $(echo $out | cut -d= -f2)
+    local out
+    out=$(env | fzf)
+    echo $(echo $out | cut -d= -f2)
 }
-
 
 # Kill process
 function fzf-kill-processes() {
-  local pid
-  pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
+    local pid
+    pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
 
-  if [ "x$pid" != "x" ]
-  then
-    echo $pid | xargs kill -${1:-9}
-  fi
+    if [ "x$pid" != "x" ]
+    then
+        echo $pid | xargs kill -${1:-9}
+    fi
 }
 
 # Enhanced Git Status (Open multiple files with tab + diff preview)
-fzf-git-status() {
+function fzf-git-status() {
     git rev-parse --git-dir > /dev/null 2>&1 || { echo "You are not in a git repository" && return }
     local selected
     selected=$(git -c color.status=always status --short |
         fzf --height 50% "$@" --border -m --ansi --nth 2..,.. \
         --preview '(git diff --color=always -- {-1} | sed 1,4d; cat {-1}) | head -500' |
         cut -c4- | sed 's/.* -> //')
-            if [[ $selected ]]; then
-                for prog in $(echo $selected);
-                do; $EDITOR $prog; done;
-            fi
-    }
+    if [[ $selected ]]; then
+        for prog in $(echo $selected);
+        do; $EDITOR $prog; done;
+    fi
+}
 
 # Checkout to existing branch or else create new branch. gco <branch-name>.
 # Falls back to fuzzy branch selector list powered by fzf if no args.
-fzf-checkout(){
+function fzf-checkout() {
     if git rev-parse --git-dir > /dev/null 2>&1; then
         if [[ "$#" -eq 0 ]]; then
             local branches branch
             branches=$(git branch -a) &&
-            branch=$(echo "$branches" |
-            fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
-            git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+                branch=$(echo "$branches" |
+                fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+                git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
         elif [ `git rev-parse --verify --quiet $*` ] || \
-             [ `git branch --remotes | grep  --extended-regexp "^[[:space:]]+origin/${*}$"` ]; then
+            [ `git branch --remotes | grep  --extended-regexp "^[[:space:]]+origin/${*}$"` ]; then
             echo "Checking out to existing branch"
             git checkout "$*"
         else
@@ -160,30 +161,30 @@ fzf-checkout(){
             git checkout -b "$*"
         fi
     else
-        echo "Can't check out or create branch. Not in a git repo"
+        echo "${RED_FG}Can't check out or create branch.${RESET} Not in a git repo"
     fi
 }
 
 # Git Braches Selecting
-fzf-git-branches(){
+function fzf-git-branches() {
     if [ -d "./.git" ]; then
-      git fetch
-      selected_remote_branch=$(git branch -r | fzf | sed -e 's/^[[:space:]]*//')
+        git fetch
+        selected_remote_branch=$(git branch -r | fzf | sed -e 's/^[[:space:]]*//')
 
-      if [ -n "$selected_remote_branch" ]; then
-        selected_branch=$(echo "$selected_remote_branch" | sed -e 's/origin\///');
+        if [ -n "$selected_remote_branch" ]; then
+            selected_branch=$(echo "$selected_remote_branch" | sed -e 's/origin\///');
 
-        if git rev-parse --verify "$selected_branch"; then
-          git checkout "$selected_branch"
+            if git rev-parse --verify "$selected_branch"; then
+                git checkout "$selected_branch"
+            else
+                git checkout --track "$selected_remote_branch"
+            fi
         else
-          git checkout --track "$selected_remote_branch"
+            echo "Exit: You haven't selected a branch..."
         fi
-      else
-        echo "Exit: You haven't selected a branch..."
-      fi
     else
-      echo "Error: There's no .git dir..."
-      exit 1
+        echo -e "${RED_FG}Error:${RESET} There's no .git dir..."
+        exit 1
     fi
 }
 
